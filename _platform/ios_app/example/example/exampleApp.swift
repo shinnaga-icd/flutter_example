65af7bdb0d34a5e0e17c3ddefa0fe8a9fa0cf6a8
shinnaga-icd <shinji.nagasaki@infcurion.com>
2023-03-02 16:25:15 +0900

    add ios

---
+++ _platform/ios_app/example/example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by 長崎 晋士 on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
