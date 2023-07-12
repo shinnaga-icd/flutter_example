c6d1b97be73391483e113f7100b44aceffd8ec2c
sinjikoro <shinnaga.dev@gmail.com>
2023-07-13 01:59:24 +0900

    feature test tips

---
+++ feature_test/lib/main_prod.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Prod',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(
+        title: 'Flutter 2nd Home Page',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
